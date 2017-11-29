e05640b7221882b61de6d55c334fabbd3125a98e
Mingyang Li <myli@big17.seas.upenn.edu>
2017-11-28 20:47:16 -0500

    updated test_surprise.py

--- test_surprise.py
+++ test_surprise.py
@@ -5,12 +5,44 @@ Created on Sun Nov 12 14:38:53 2017
 
 @author: lmy
 """
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
-from surprise import SVD
+from surprise import SVD, GridSearch
 from surprise import Dataset, Reader
 from surprise import evaluate, print_perf
-
+from pprint import pprint as pp
 import pandas as pd
+from tabulate import tabulate
+
+#%% Configurations
+
+use_mini = False
+verbose  = True
+random_state = 42 # Using 42, because it's the Answer to the Ultimate Question of Life, the Universe, and Everything.
+
+
+#%% my print function
+
+import datetime
+from pprint import pformat as pf
+dt = lambda: datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
+filename = datetime.datetime.now().strftime("Output %Y-%m-%d %H-%M-%S.txt")
+def p(*argv):
+    if not verbose: return
+    string = dt()
+    for i in argv:
+        if type(i) == str:
+            string += ' '+i
+        else:
+            string += ' '+pf(i)
+    with open(filename, 'a') as f:
+        print(string, file = f)
+    print(string)
+
+
+#%% Development -- optimize the classifier!
 
 #remove_non_numerical_rows_in_column = lambda df, col: df[df[col].apply(lambda x: x.isnumeric())]
 
@@ -20,7 +52,7 @@ user_ratings_df = pd.read_csv('getting_scores/listings_with_score.csv', usecols=
 # remove rows with non-numerical values in the column "Polarity":
 user_ratings_df = user_ratings_df[pd.to_numeric(user_ratings_df['Polarity'], errors='coerce').notnull()]
 
-# A reader is still needed but only the rating_scale param is requiered.
+# A reader is still needed but only the rating_scale param is required.
 reader = Reader(rating_scale=(-1, 1))
 
 # The columns must correspond to user id, item id and ratings (in that order).
@@ -32,20 +64,39 @@ data.split(n_folds=3)
 # We'll use the famous SVD algorithm.
 algo = SVD()
 
-# Evaluate performances of our algorithm on the dataset.
-perf = evaluate(algo, data, measures=['RMSE', 'MAE'])
+param_grid = {'n_epochs': [5, 10],
+	      'lr_all': [0.002, 0.005],
+              'reg_all': [0.4, 0.6]}
 
-print_perf(perf)
+grid_search = GridSearch(SVD, param_grid, measures=['RMSE'])#, 'FCP'])
+
+# Evaluate performances of our algorithm on the dataset.
+#perf = evaluate(algo, data, measures=['RMSE', 'MAE'])
+
+#print_perf(perf)
+
+# Evaluate performances of our algorithm on the dataset.
+perf = grid_search.evaluate(data)
+#print_perf(perf)
+
+#pp(dict(grid_search.best_score))
+#pp(dict(grid_search.best_params))
+#pp(dict(grid_search.best_estimator))x
+
+#%% Production!
+
+grid_search.best_score['RMSE']
 
 trainset = data.build_full_trainset()
 algo.train(trainset)
 
-
 all_listings = user_ratings_df['listing_id'].unique()
 
 enduserID = str(19634955)
-
-results = pd.DataFrame({'listingID': all_listings, 
-                        'rating': [algo.predict(enduserID, str(i)).est for i in all_listings]}).sort_values('rating', ascending=False)
-
-print results.head()
+print('Hello, User #'+enduserID+'.')
+results = pd.DataFrame({'Listing ID': all_listings, 
+                        'Predicted Rating From You': [algo.predict(enduserID, str(i)).est for i in all_listings]})\
+         .sort_values('Predicted Rating From You', ascending=False)
+print('Based on your previous ratings, here are our suggestions for your future stay:')
+print(tabulate(results.head(), headers='keys', showindex=False, tablefmt="fancy_grid"))
+print('(root-mean-square error (RMSE): ',grid_search.best_score['RMSE'], ', better smaller.)')

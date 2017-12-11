32526c31c2dfe6fdf7f013852af2d275197a4274
Mingyang Li <tslmy@outlook.com>
2017-12-11 18:45:50 -0500

    Viz inline

--- pipeline.py
+++ pipeline.py
@@ -15,38 +15,61 @@ from tabulate import tabulate
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.neighbors import KNeighborsClassifier as KNN
 
+# for plotting
+import seaborn as sns
+from matplotlib.patches import Rectangle
+import matplotlib.pyplot as plt
+
 #%% Configurations
 
 verbose  = True
 random_state = 42 # Using 42, because it's the Answer to the Ultimate Question of Life, the Universe, and Everything.
-destin_city = 'Paris'
+destin_city = 'New York'
 
 # Paths here
-your_ratings_to_listings_csv  = 'datasets/recommender_system/enduser_listings_ratings.csv'
-eigenmatrix_csv = 'getting_scores/user_cluster_rating_per_listing_cluster.csv'
-user_groups_csv = 'getting_scores/reviewers_rating_per_listing_cluster.csv'
-listing_clusters_csv = 'datasets/All_listings/clusterized_listings_with_AMN.csv'
-listings_details_csv = 'datasets/All_listings/sample_listings.csv'
-all_listings_csv = 'datasets/All_listings/all_listings.csv'
-sample_listings_amenities_csv = 'datasets/All_listings/sample_listings_amenities.csv'
+your_ratings_to_listings_csv    = 'datasets/recommender_system/enduser_listings_ratings.csv'
+eigenmatrix_csv                 = 'getting_scores/user_cluster_rating_per_listing_cluster.csv'
+user_groups_csv                 = 'getting_scores/reviewers_rating_per_listing_cluster.csv'
+listing_clusters_csv            = 'datasets/All_listings/clusterized_listings_with_AMN.csv'
+listings_details_csv            = 'datasets/All_listings/sample_listings.csv'
+all_listings_csv                = 'datasets/All_listings/all_listings.csv'
+sample_listings_amenities_csv   = 'datasets/All_listings/sample_listings_amenities.csv'
 #sample_listings_amenities_expanded_csv = 'datasets/All_listings/sample_listings_amenities_expanded.csv'
 #sample_listings_amenities_expanded_npz = 'datasets/All_listings/sample_listings_amenities_expanded.npz'
-PCA_loadings_csv = 'datasets/All_listings/sample_AMN_PCA_U.csv'
-listings_with_score_csv = 'getting_scores/listings_with_score.csv'
+PCA_loadings_csv                = 'datasets/All_listings/sample_AMN_PCA_U.csv'
+listings_with_score_csv         = 'getting_scores/listings_with_score.csv'
 
-amenity_vectorizer_pkl = 'amenity_vectorizer.pkl'
+amenity_vectorizer_pkl          = 'amenity_vectorizer.pkl'
 
 #%% my print function
 
-p = print
+# =============================================================================
+# import datetime
+# from pprint import pformat as pf
+# dt = lambda: datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
+# filename = datetime.datetime.now().strftime("Output %Y-%m-%d %H-%M-%S.txt")
+# def p(*argv):
+#     if not verbose: return
+#     string = dt()
+#     for i in argv:
+#         if type(i) == str:
+#             string += ' '+i
+#         else:
+#             string += ' '+pf(i)
+#     with open(filename, 'a') as f:
+#         print(string, file = f)
+#     print(string)
+# 
+# =============================================================================
 
+p = print
 #%% Read datasets:
 
 user_groups_df  = pd.read_csv(user_groups_csv, encoding='utf-8', index_col = 'reviewer_id').rename(columns = {'clusters': 'groupID'})
 eigenmatrix_df  = pd.read_csv(eigenmatrix_csv, encoding='utf-8').drop('user_cluster', axis=1)
 your_ratings_to_listings_df = pd.read_csv(your_ratings_to_listings_csv, encoding='utf-8')
 listing_clusters_df         = pd.read_csv(listing_clusters_csv, encoding='utf-8')
-listing_details_df          = pd.read_csv(listings_details_csv, encoding='utf-8', index_col = 'id', usecols = ['id', 'name', 'city'])
+listing_details_df          = pd.read_csv(listings_details_csv, encoding='utf-8', index_col = 'id', usecols = ['id', 'name', 'city', 'thumbnail_url', 'listing_url', 'latitude', 'longitude'])
 PCA_loadings_df = pd.read_csv(PCA_loadings_csv, encoding='utf-8')
 amn_all_col = pd.read_csv(all_listings_csv, usecols=['id','amenities'], index_col='id')['amenities'].dropna().str[1:-1]
 listings_with_score_df = pd.read_csv(listings_with_score_csv, encoding='utf-8', header=0, 
@@ -82,6 +105,7 @@ else:
 all_columns = ["PC1","PC2","PC3","PC4","PC5","PC6","PC7","PC8","PC9","PC10","PC11","PC12","PC13","PC14","PC15","PC16","PC17","PC18","PC19","PC20"]
 
 def listing_clusterer(your_ratings_to_listings):
+    #global enduser_preference_profile, enduser_cluster_labels
     #MERGE ENDUSER PREFERENCES WITH AMENITIES
     enduser_listings_with_AMN = amn_all_col.loc[your_ratings_to_listings.index]
     enduser_listings_with_AMN_binary = pd.DataFrame(
@@ -93,17 +117,31 @@ def listing_clusterer(your_ratings_to_listings):
     enduser_PCAs_with_cluster = pd.concat([enduser_cluster_labels,your_ratings_to_listings],axis=1)
     # now group by clusterID:
     enduser_preference_profile = pd.DataFrame(enduser_PCAs_with_cluster.groupby(['clusterID'])['ratings'].mean())
+    enduser_cluster_labels.clusterID.value_counts().plot.pie(label='Cluster ID', figsize=(2, 2),autopct='%d%%').set_title('Listings You Rated Grouped By Cluster')
+    centre_circle = plt.Circle((0,0),0.75,color='white', fc='white',linewidth=0)
+    fig = plt.gcf()
+    fig.gca().add_artist(centre_circle)
     return enduser_preference_profile['ratings']
+
+def autolabel(rects, ax):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., height,
+                 "%.2f" % (height),
+                ha='center', va='top')
 
 #%% simulate enduser input:
 
 p('Please enter a list of listings and your ratings of them:')
 your_ratings_to_listings = pd.read_csv(your_ratings_to_listings_csv, index_col = 'listing_id').ratings
 p('-'*30, '\n', your_ratings_to_listings, '\n', '-'*30)
-
-p('Based on your input, we think that you have the following ratings to these types of places:')
+p('Listings you visited, shown as proportions of clusters:')
 your_ratings_to_clusters = listing_clusterer(your_ratings_to_listings)
-p(tabulate(pd.DataFrame(your_ratings_to_clusters), headers=['Cluster ID', 'Estimated Rating From You'], tablefmt="fancy_grid"))
+#p('Based on your input, we think that you have the following ratings to these types of places:')
+#p(tabulate(pd.DataFrame(your_ratings_to_clusters), headers=['Cluster ID', 'Estimated Rating From You'], tablefmt="fancy_grid"))
 
 
 #%% Identify which group this enduser belongs to:
@@ -115,6 +153,26 @@ p('Hence, we think that you are one of the group', your_groupID, '.')
 your_peerIDs = user_groups_df[ user_groups_df.groupID == your_groupID ].index
 p('You have', len(your_peerIDs), 'peers.')
 your_clusterID = int(eigenmatrix_df.loc[your_groupID].idxmax())
+ratings_from_peers = eigenmatrix_df.loc[your_groupID]
+#%% draw ratings to clusters:
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
+bar1 = ax1.bar(ratings_from_peers.index, ratings_from_peers, label='Your Peers')
+bar2 = ax1.bar(your_ratings_to_clusters.index, your_ratings_to_clusters, label='You')
+ax1.legend((bar1, bar2), ('Your Peers', 'You'))
+autolabel(bar1, ax=ax1)
+autolabel(bar2, ax=ax1)
+ax1.set_xlabel('Listing Cluster ID')
+ax1.set_ylabel('Rating')
+ax1.set_title('Ratings From Your User Group To Each Listing Cluster')
+
+hm = sns.heatmap(eigenmatrix_df, annot=True, fmt=".2f", square=True)
+hm.add_patch(Rectangle((0, your_groupID), len(ratings_from_peers), 1, fill=False, edgecolor='blue', lw=3))
+ax2.set_xlabel('Listing Cluster ID')
+ax2.set_ylabel('User Group ID')
+ax2.set_title('Ratings From Each User Group To Each Listing Cluster')
+plt.show()
+
 p('You people love listings in cluster', your_clusterID, '.')
 
 #%% Sort clusters by ratings from this group:
@@ -146,6 +204,54 @@ picked_listings_details_df = listing_details_df.loc[listingIDs_in_this_cluster]
 picked_listings_details_df = picked_listings_details_df[picked_listings_details_df.city == destin_city].drop('city', axis=1)
 # Append the column of ratings:
 picked_listings_details_df['rating'] = listings_with_score[picked_listings_details_df.index]
+picked_listings_details_df['visual'] = ['|'*int(10*i) for i in picked_listings_details_df['rating']]
 # Output:
-p('For your coming stay in', destin_city, 'May I suggest:')
-p(tabulate(picked_listings_details_df.head(), headers=['Name', 'Average Rating From Your Peers'], showindex=False, tablefmt="fancy_grid"))
+p('For your coming stay in', destin_city, ', may I suggest:')
+p(tabulate(picked_listings_details_df[['name', 'rating', 'visual']].head(), headers=['Name', '', 'Average Rating From Your Peers'], showindex=False, tablefmt="fancy_grid"))
+#first_5_images = picked_listings_details_df.thumbnail_url.fillna('http://via.placeholder.com/216x144').head()
+#for url in first_5_images:
+#    Image(url.split('?')[0]).show()
+p(picked_listings_details_df.listing_url.head())
+#%% Development -- optimize the classifier!
+# =============================================================================
+# 
+# data_df = listings_with_score_df\
+# 	.merge(user_groups_df[['groupID']], left_on='reviewer_id' ,right_index=True)\
+# 	.merge(listing_clusters_df[['id', 'listing_AMN_cluster']], 
+# 			left_on='listing_id', right_on='id')\
+# 	.rename(columns={'listing_AMN_cluster': 'clusterID','Polarity': 'rating'})\
+# 	[['groupID', 'clusterID', 'rating']]\
+# 	.sort_values('rating')\
+# 	.astype({'clusterID': int})
+# 
+# import surprise
+# 
+# #remove_non_numerical_rows_in_column = lambda df, col: df[df[col].apply(lambda x: x.isnumeric())]
+# 
+# # Read data:
+# # A reader is still needed but only the rating_scale param is required.
+# reader = surprise.Reader(rating_scale=(-1, 1))
+# 
+# # The columns must correspond to user id, item id and ratings (in that order).
+# data = surprise.Dataset.load_from_df(data_df, reader)
+# 
+# # split it into 3 folds for cross-validation.
+# data.split(n_folds=3)
+# 
+# 
+# #pp(dict(grid_search.best_score))
+# #pp(dict(grid_search.best_params))
+# #pp(dict(grid_search.best_estimator))
+# #%% test every algo:
+# surprise.print_perf(  surprise.evaluate(surprise.KNNBasic(), data, measures=['RMSE', 'MAE', 'FCP'])  )
+# surprise.print_perf(  surprise.evaluate(surprise.KNNWithMeans(), data, measures=['RMSE', 'MAE', 'FCP'])  )
+# surprise.print_perf(  surprise.evaluate(surprise.KNNWithZScore(), data, measures=['RMSE', 'MAE', 'FCP'])  )
+# surprise.print_perf(  surprise.evaluate(surprise.KNNBaseline(), data, measures=['RMSE', 'MAE', 'FCP'])  )
+# surprise.print_perf(  surprise.evaluate(surprise.SVD(), data, measures=['RMSE', 'MAE', 'FCP'])  )
+# surprise.print_perf(  surprise.evaluate(surprise.SVDpp(), data, measures=['RMSE', 'MAE', 'FCP'])  )
+# surprise.print_perf(  surprise.evaluate(surprise.NMF(), data, measures=['RMSE', 'MAE', 'FCP'])  )
+# surprise.print_perf(  surprise.evaluate(surprise.SlopeOne(), data, measures=['RMSE', 'MAE', 'FCP'])  )
+# surprise.print_perf(  surprise.evaluate(surprise.CoClustering(), data, measures=['RMSE', 'MAE', 'FCP'])  )
+# 
+# 
+# =============================================================================

e2ce2f3c7e6e313d262d049721c8314ef5595dfa
Mingyang Li <tslmy@outlook.com>
2017-12-11 20:54:37 -0500

    Updated viz

--- pipeline.py
+++ pipeline.py
@@ -14,7 +14,7 @@ import pickle
 from tabulate import tabulate
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.neighbors import KNeighborsClassifier as KNN
-
+from random import choice
 # for plotting
 import seaborn as sns
 from matplotlib.patches import Rectangle
@@ -66,6 +66,7 @@ p = print
 
 user_groups_df  = pd.read_csv(user_groups_csv, encoding='utf-8', index_col = 'reviewer_id').rename(columns = {'clusters': 'groupID'})
 eigenmatrix_df  = pd.read_csv(eigenmatrix_csv, encoding='utf-8').drop('user_cluster', axis=1)
+eigenmatrix_df.index = pd.to_numeric(eigenmatrix_df.index)
 your_ratings_to_listings_df = pd.read_csv(your_ratings_to_listings_csv, encoding='utf-8')
 listing_clusters_df         = pd.read_csv(listing_clusters_csv, encoding='utf-8')
 listing_details_df          = pd.read_csv(listings_details_csv, encoding='utf-8', index_col = 'id', usecols = ['id', 'name', 'city', 'thumbnail_url', 'listing_url', 'latitude', 'longitude'])
@@ -122,7 +123,7 @@ def listing_clusterer(your_ratings_to_listings):
     fig.gca().add_artist(centre_circle)
     return enduser_preference_profile['ratings']
 
-def autolabel(rects, ax):
+def autolabel(rects, ax, c):
     """
     Attach a text label above each bar displaying its height
     """
@@ -130,53 +131,7 @@ def autolabel(rects, ax):
         height = rect.get_height()
         ax.text(rect.get_x() + rect.get_width()/2., height,
                  "%.2f" % (height),
-                ha='center', va='top')
-
-#%% simulate enduser input:
-
-p('Please enter a list of listings and your ratings of them:')
-your_ratings_to_listings = pd.read_csv(your_ratings_to_listings_csv, index_col = 'listing_id').ratings
-p('-'*30, '\n', your_ratings_to_listings, '\n', '-'*30)
-p('Listings you visited, shown as proportions of clusters:')
-your_ratings_to_clusters = listing_clusterer(your_ratings_to_listings)
-#p('Based on your input, we think that you have the following ratings to these types of places:')
-#p(tabulate(pd.DataFrame(your_ratings_to_clusters), headers=['Cluster ID', 'Estimated Rating From You'], tablefmt="fancy_grid"))
-
-
-#%% Identify which group this enduser belongs to:
-clusters_you_rated = your_ratings_to_clusters.index
-eigenmatrix_reduced_df = eigenmatrix_df.iloc[:,clusters_you_rated]
-users_grouper = KNN(n_neighbors=1).fit(X = eigenmatrix_reduced_df, y = eigenmatrix_reduced_df.index)
-your_groupID = users_grouper.predict(np.array(clusters_you_rated).reshape(1, -1))[0]
-p('Hence, we think that you are one of the group', your_groupID, '.')
-your_peerIDs = user_groups_df[ user_groups_df.groupID == your_groupID ].index
-p('You have', len(your_peerIDs), 'peers.')
-your_clusterID = int(eigenmatrix_df.loc[your_groupID].idxmax())
-ratings_from_peers = eigenmatrix_df.loc[your_groupID]
-#%% draw ratings to clusters:
-
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
-bar1 = ax1.bar(ratings_from_peers.index, ratings_from_peers, label='Your Peers')
-bar2 = ax1.bar(your_ratings_to_clusters.index, your_ratings_to_clusters, label='You')
-ax1.legend((bar1, bar2), ('Your Peers', 'You'))
-autolabel(bar1, ax=ax1)
-autolabel(bar2, ax=ax1)
-ax1.set_xlabel('Listing Cluster ID')
-ax1.set_ylabel('Rating')
-ax1.set_title('Ratings From Your User Group To Each Listing Cluster')
-
-hm = sns.heatmap(eigenmatrix_df, annot=True, fmt=".2f", square=True)
-hm.add_patch(Rectangle((0, your_groupID), len(ratings_from_peers), 1, fill=False, edgecolor='blue', lw=3))
-ax2.set_xlabel('Listing Cluster ID')
-ax2.set_ylabel('User Group ID')
-ax2.set_title('Ratings From Each User Group To Each Listing Cluster')
-plt.show()
-
-p('You people love listings in cluster', your_clusterID, '.')
-
-#%% Sort clusters by ratings from this group:
-
-destin_city = 'New York'
+                ha='center', va='top', color = c)
 
 def get_ranking(your_groupID,your_clusterID):
     '''Returns the list of listingIDs sorted to their ratings as provided by users specified peerIDs.'''
@@ -195,6 +150,71 @@ def get_ranking(your_groupID,your_clusterID):
     listings_with_score = listings_with_score[listings_with_score.reviewer_id.isin(peersIDs)]
     # average_ratings_of_suggested_listings_by_peers :
     return listings_with_score.groupby(['listing_id'])['Polarity'].mean().sort_values(ascending=False) 
+
+#%% simulate enduser input:
+
+p('Please enter a list of listings and your ratings of them:')
+random_enduser = True
+if random_enduser:
+    num_listings_enduser_rated = np.random.randint(5,15)
+    your_ratings_to_listings = pd.DataFrame(data={'ratings':np.random.random(num_listings_enduser_rated), 
+                                                  'listing_id': amn_all_col.index.to_series().sample(num_listings_enduser_rated).astype(float)}).ratings
+else:
+    your_ratings_to_listings = pd.read_csv(your_ratings_to_listings_csv, index_col = 'listing_id').ratings
+p('-'*30, '\n', your_ratings_to_listings, '\n', '-'*30)
+p('Listings you visited, shown as proportions of clusters:')
+your_ratings_to_clusters = listing_clusterer(your_ratings_to_listings)
+#p('Based on your input, we think that you have the following ratings to these types of places:')
+#p(tabulate(pd.DataFrame(your_ratings_to_clusters), headers=['Cluster ID', 'Estimated Rating From You'], tablefmt="fancy_grid"))
+
+
+# Identify which group this enduser belongs to:
+clusters_you_rated = your_ratings_to_clusters.index
+eigenmatrix_reduced_df = eigenmatrix_df.iloc[:,clusters_you_rated]
+users_grouper = KNN(n_neighbors=1).fit(X = eigenmatrix_reduced_df, y = eigenmatrix_reduced_df.index)
+your_groupID = users_grouper.predict(np.array(clusters_you_rated).reshape(1, -1))[0]
+p('Hence, we think that you are one of the group', your_groupID, '.')
+your_peerIDs = user_groups_df[ user_groups_df.groupID == your_groupID ].index
+p('You have', len(your_peerIDs), 'peers.')
+your_clusterID = int(eigenmatrix_df.loc[your_groupID].idxmax())
+ratings_from_peers = eigenmatrix_df.loc[your_groupID]
+
+# draw ratings to clusters:
+
+cluster_ratings_from_all_groups = eigenmatrix_df.mean()
+clusterIDs = pd.to_numeric(cluster_ratings_from_all_groups.index)
+num_clusters = len(clusterIDs)
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
+colormap = plt.get_cmap('terrain')
+bar1 = ax1.bar(clusterIDs, ratings_from_peers, width=.8, label='Your Peers', 
+               facecolor=[1,0,0,.5])
+bar2 = ax1.bar(your_ratings_to_clusters.index, your_ratings_to_clusters, width=.9, label='You', 
+               facecolor=[0,1,0,.5])
+bar3 = ax1.bar(clusterIDs, cluster_ratings_from_all_groups, width=.7, label='All Users', 
+               facecolor=[0,0,1,.5])
+ax1.legend((bar1, bar2, bar3), ('Your Peers', 'You', 'All Users'), loc='best')
+autolabel(bar1, ax=ax1, c='white')
+autolabel(bar2, ax=ax1, c='white')
+autolabel(bar3, ax=ax1, c='white')
+ax1.set_xlabel('Listing Cluster ID')
+ax1.set_ylabel('Rating')
+ax1.set_ylim(eigenmatrix_df.min().min(), eigenmatrix_df.max().max())
+ax1.set_title('Ratings From Your User Group To Each Listing Cluster')
+
+hm = sns.heatmap(eigenmatrix_df, annot=True, fmt=".2f", square=True)
+hm.add_patch(Rectangle((0, your_groupID), len(ratings_from_peers), 1, fill=False, edgecolor='blue', lw=3))
+ax2.set_xlabel('Listing Cluster ID')
+ax2.set_ylabel('User Group ID')
+ax2.set_title('Ratings From Each User Group To Each Listing Cluster')
+plt.show()
+
+p('You people love listings in cluster', your_clusterID, '.')
+
+# Sort clusters by ratings from this group:
+
+destin_city = choice(['New York', 'London', 'Paris'])
+
 # Sort listings in this cluster by ratings according to peers in this group:
 listings_with_score = get_ranking(your_clusterID, your_groupID)
 # Get a unique list of all suggested listings' IDs:
@@ -213,46 +233,3 @@ p(tabulate(picked_listings_details_df[['name', 'rating', 'visual']].head(), head
 #for url in first_5_images:
 #    Image(url.split('?')[0]).show()
 p(picked_listings_details_df.listing_url.head())
-#%% Development -- optimize the classifier!
-# =============================================================================
-# 
-# data_df = listings_with_score_df\
-# 	.merge(user_groups_df[['groupID']], left_on='reviewer_id' ,right_index=True)\
-# 	.merge(listing_clusters_df[['id', 'listing_AMN_cluster']], 
-# 			left_on='listing_id', right_on='id')\
-# 	.rename(columns={'listing_AMN_cluster': 'clusterID','Polarity': 'rating'})\
-# 	[['groupID', 'clusterID', 'rating']]\
-# 	.sort_values('rating')\
-# 	.astype({'clusterID': int})
-# 
-# import surprise
-# 
-# #remove_non_numerical_rows_in_column = lambda df, col: df[df[col].apply(lambda x: x.isnumeric())]
-# 
-# # Read data:
-# # A reader is still needed but only the rating_scale param is required.
-# reader = surprise.Reader(rating_scale=(-1, 1))
-# 
-# # The columns must correspond to user id, item id and ratings (in that order).
-# data = surprise.Dataset.load_from_df(data_df, reader)
-# 
-# # split it into 3 folds for cross-validation.
-# data.split(n_folds=3)
-# 
-# 
-# #pp(dict(grid_search.best_score))
-# #pp(dict(grid_search.best_params))
-# #pp(dict(grid_search.best_estimator))
-# #%% test every algo:
-# surprise.print_perf(  surprise.evaluate(surprise.KNNBasic(), data, measures=['RMSE', 'MAE', 'FCP'])  )
-# surprise.print_perf(  surprise.evaluate(surprise.KNNWithMeans(), data, measures=['RMSE', 'MAE', 'FCP'])  )
-# surprise.print_perf(  surprise.evaluate(surprise.KNNWithZScore(), data, measures=['RMSE', 'MAE', 'FCP'])  )
-# surprise.print_perf(  surprise.evaluate(surprise.KNNBaseline(), data, measures=['RMSE', 'MAE', 'FCP'])  )
-# surprise.print_perf(  surprise.evaluate(surprise.SVD(), data, measures=['RMSE', 'MAE', 'FCP'])  )
-# surprise.print_perf(  surprise.evaluate(surprise.SVDpp(), data, measures=['RMSE', 'MAE', 'FCP'])  )
-# surprise.print_perf(  surprise.evaluate(surprise.NMF(), data, measures=['RMSE', 'MAE', 'FCP'])  )
-# surprise.print_perf(  surprise.evaluate(surprise.SlopeOne(), data, measures=['RMSE', 'MAE', 'FCP'])  )
-# surprise.print_perf(  surprise.evaluate(surprise.CoClustering(), data, measures=['RMSE', 'MAE', 'FCP'])  )
-# 
-# 
-# =============================================================================
